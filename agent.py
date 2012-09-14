ba13f870d28bae9686ccf5bb3422e6bbe841980c
James Kingsbery <06jameskingsbery@gmail.com>
2012-09-13 23:04:24 -0400

    Reworked agents a bit so that buyers are price takers, monopoly determines price based on desired consumption

--- agent.py
+++ agent.py
@@ -16,30 +16,32 @@ class Agent:
 
 class BuyingAgent:
     def __init__(self):
-        self.quantity=10
-        self.most_wanted=100
+        self.quantity=2
         self.reserve_price=max(random.normalvariate(8,2),0)
         print "Agent reserve price " + str(self.reserve_price)
-        self.price=6
 
-    def update(self):
-        if self.price<self.reserve_price and self.quantity<self.most_wanted:
-            self.quantity+=1
-            self.price+=1
+    def quantityWanted(self,price):
+        m=-1
+        b=20
+        return max(0,(price-b)/m)
+
+    def update(self,new_price):
+        if new_price<self.reserve_price:
+            self.quantity=self.quantityWanted(self.reserve_price)
+        else:
+            self.quantity=0
         
 class SellingAgent:
     def __init__(self):
         self.b=0
         self.m=1
-        self.quantity=4
+        self.quantity=0
         self.price=self.marginalCost(self.quantity)
         self.last_revenue=0
 
     def fillOrders(self,buyingAgents):
-        buying=filter(lambda x:x.price>=self.price,buyingAgents)
-        sold=sum(map(lambda x:x.quantity,buying))
-        print buying
-        self.last_revenue=sum(map(lambda x:x.price*x.quantity,buying))
+        sold=sum(map(lambda x:x.quantity,buyingAgents))
+        self.last_revenue=sum(map(lambda x:self.price*x.quantity,buyingAgents))
 
         if self.quantity>sold:
             self.quantity-=0.1
@@ -57,15 +59,19 @@ class SellingAgent:
 
 if __name__=="__main__":
     random.seed(13)
-    NUM_ITERS=100
-    NUM_BUYERS=2
+    NUM_ITERS=10000
+    NUM_BUYERS=20
     selling=SellingAgent()
     buyers=[]
+
     for i in range(NUM_BUYERS):
         buyers.append(BuyingAgent())
 
     for iter in range(NUM_ITERS):
         selling.fillOrders(buyers)
         for x in buyers:
-            x.update()
-        print selling
+            x.update(selling.price)
+
+    print selling
+
+    
